828bdf731dbe92937e187f97b0a1ed2373b95715
Ulrich Stark <pwned-pixel@posteo.de>
2025-08-28 19:39:23 +0200

    qualcommax: ipq807x: ZyXEL NBG7815: DualBoot Support (1)
    In order to change the bootconfig it is necessary to modify the DTS file.
    mtd2 and mtd3 have to be writeable.
    
    ZyXEL is writing both bootpartitions mtd2/0:bootconfig and mtd3/0:bootconfig1
    every time during firmware update.
    
    As we've installed OpenWrt accordingly what is written in the Wiki we only
    changed the bootconfig for "rootfs" (seek 168). Which is basically wrong.
    
    Flashing ZyXEL firmware via tftp is fixing this order. Because they
    are writing the order for kernel and wififw too. Always accordingly what
    rootfs is giving them and regardless how often you flash the firmware over
    the device through either gui or tftp.
    They are changing always this three partitions. The other partitions are not
    touched and managed by the bootloader/zloader for failsafe boot.
    
    cat /proc/boot_info/*/upgradepartition
    
    0:APDP_1
    0:APPSBL_1
    0:CDT_1
    0:DEVCFG_1
    0:HLOS
    0:QSEE_1
    0:RPM_1
    0:WIFIFW
    rootfs
    
    cat /proc/boot_info/*/primaryboot
    
    0
    0
    0
    0
    1
    0
    0
    1
    1
    
    1 is here kernel, wififw and rootfs. The 1 is reflected accordingly what's
    written within upgradepartition.
    
    Within the partition /dev/mtd[2,3] the order is not alphabetical like the
    output above.
    
    0:QSEE
    0:DEVCFG
    0:APDP
    0:RPM
    0:CDT
    0:APPSBL
    0:HLOS
    rootfs
    0:WIFIFW
    
    --> seek 148=kernel, 168=rootfs, 188=wififw
    
    So for upgrading OpenWrt writing the opposite partition we are modifying
    this three parts of the bootconfig like ZyXEL is doing it.

--- target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
@@ -300,15 +300,35 @@ platform_do_upgrade() {
 		echo "0" > /sys/class/leds/q1:channel1/device/run_engine
 		echo "00499C0040FF7E0040007E00A001" > /sys/class/leds/q1:channel1/device/engine1_load
 		echo "1" > /sys/class/leds/q1:channel1/device/run_engine
-		local config_mtdnum="$(find_mtd_index 0:bootconfig)"
-		[ -z "$config_mtdnum" ] && reboot
-		part_num="$(hexdump -e '1/1 "%01x|"' -n 1 -s 168 -C /dev/mtd$config_mtdnum | cut -f 1 -d "|" | head -n1)"
-		if [ "$part_num" -eq "0" ]; then
-			CI_KERNPART="0:HLOS"
-			CI_ROOTPART="rootfs"
-		else
+		local mtd_part_2="mtd""$(find_mtd_index 0:bootconfig)"
+		local mtd_part_3="mtd""$(find_mtd_index 0:bootconfig1)"
+		[ -z "$mtd_part_2" ] && reboot
+		[ -z "$mtd_part_3" ] && reboot
+		local bootconfig0rootfs=$(hexdump -v -e '1/1 "%01x|"' -n 1 -s 168 -C /dev/"$mtd_part_2" | cut -f 1 -d "|" | head -n1)
+		prepare_bootconfig() {
+			rm /tmp/"$mtd_part_2".bin
+			dd if=/dev/"$mtd_part_2" of=/tmp/"$mtd_part_2".bin bs=336 count=1
+			offset=$(echo 148 168 188)
+			if [ "${bootconfig0rootfs}" -eq 0 ]; then
+				for i in $offset; do printf '\x01' | dd of=/tmp/"$mtd_part_2".bin bs=1 seek="$i" count=1 conv=notrunc; done
+			else
+				for i in $offset; do printf '\x00' | dd of=/tmp/"$mtd_part_2".bin bs=1 seek="$i" count=1 conv=notrunc; done
+			fi
+		}
+		prepare_bootconfig
+		write_bootconfig() {
+			dd if=/tmp/"$mtd_part_2".bin 2>/dev/null | mtd -e /dev/"$mtd_part_2" write - /dev/"$mtd_part_2" 2>/dev/null
+			dd if=/tmp/"$mtd_part_2".bin 2>/dev/null | mtd -e /dev/"$mtd_part_3" write - /dev/"$mtd_part_3" 2>/dev/null
+			sync
+		}
+		if [ "$bootconfig0rootfs" -eq "0" ]; then
+			write_bootconfig
 			CI_KERNPART="0:HLOS_1"
 			CI_ROOTPART="rootfs_1"
+		else
+			write_bootconfig
+			CI_KERNPART="0:HLOS"
+			CI_ROOTPART="rootfs"
 		fi
 		emmc_do_upgrade "$1"
 		;;
